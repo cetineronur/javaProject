c230d5d560e2a201159ecc800307130bf962204c
onur cetiner <93998298+cetineronur@users.noreply.github.com>
2021-12-27 10:50:48 +0100

    wurde andere Verzeichnis umgezogen

--- bin/tag05_mathematischeOperationen/matematikselIslemler.java
+++
@@ -1,9 +0,0 @@
-package tag05_mathematischeOperationen;
-public class matematikselIslemler {
-	public static void main(String[] args) {
-		double a=3;
-		System.out.println(4*(2+5)/a);
-
-	}
-
-}
--- bin/tag05_mathematischeOperationen/modulus.java
+++
@@ -1,7 +0,0 @@
-package tag05_mathematischeOperationen;
-public class modulus {
-	public static void main(String[] args) {
-		int a=15%4;
-		System.out.println(a);
-	}
-}
